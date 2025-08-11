2e5bd7711e530949b43e7e6805d8bb703e2b0748
Amir khodadi <amiramkhodadi@gmail.com>
2025-08-11 22:48:50 +0330

    add change password feture and some debug code

--- account/urls.py
+++ account/urls.py
@@ -1,10 +1,12 @@
 from django.urls import path
 from .import views
-from django.contrib.auth.views import LoginView,LogoutView
-app_name = 'account'
+from django.contrib.auth.views import LoginView,LogoutView,PasswordChangeView,PasswordChangeDoneView
+# app_name = 'account'
 urlpatterns=[
     # path('login/' ,views.user_login , name ='login'),
     path('login/' ,LoginView.as_view() , name ='login'),
     path('logout/' ,LogoutView.as_view() , name ='logout'),
     path('', views.dashboard, name='dashboard'),
+    path('password-change/', PasswordChangeView.as_view(), name='password_change'),
+    path('password-change/done/', PasswordChangeDoneView.as_view(), name='password_change_done'),
 ]
--- config/settings.py
+++ config/settings.py
@@ -123,6 +123,6 @@ STATIC_URL = '/static/'
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
-LOGIN_REDIRECT_URL = 'account:dashboard'
-LOGIN_URL = 'account:login'
-LOGOUT_URL = 'account:logout'
+LOGIN_REDIRECT_URL = 'dashboard'
+LOGIN_URL = 'login'
+LOGOUT_URL = 'logout'

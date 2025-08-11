5dfe58a30aa8472ce52f35f6cb2562e15d82d4c4
Amir khodadi <amiramkhodadi@gmail.com>
2025-08-11 17:53:29 +0330

    use LoginView and LogoutView for user authentication with standard name

--- account/urls.py
+++ account/urls.py
@@ -1,7 +1,10 @@
 from django.urls import path
 from .import views
-
+from django.contrib.auth.views import LoginView,LogoutView
 app_name = 'account'
 urlpatterns=[
-    path('login/' ,views.user_login , name ='login'),
+    # path('login/' ,views.user_login , name ='login'),
+    path('login/' ,LoginView.as_view() , name ='login'),
+    path('logout/' ,LogoutView.as_view() , name ='logout'),
+    path('', views.dashboard, name='dashboard'),
 ]
--- account/views.py
+++ account/views.py
@@ -2,28 +2,38 @@ from django.shortcuts import render
 from .forms import LoginForm
 from django.contrib.auth import authenticate,login
 from django.http import HttpResponse
+# from django.contrib.auth.views  import LoginView,LogoutView
 # Create your views here.
-def user_login(request):
-    if request.method=='POST':
-        form = LoginForm(request.POST)
-        if form.is_valid():
-            username = form.cleaned_data['username']
-            password = form.cleaned_data['password']
-            user = authenticate(request, username=username , password = password)
-            if user :
+# def user_login(request):
+#     if request.method=='POST':
+#         form = LoginForm(request.POST)
+#         if form.is_valid():
+#             username = form.cleaned_data['username']
+#             password = form.cleaned_data['password']
+#             user = authenticate(request, username=username , password = password)
+#             if user :
+#
+#                 if user.is_active :
+#                     login(request,user)
+#                     return HttpResponse("YOU ARE LOGIN ;) ")
+#                 else:
+#                     return HttpResponse("YOU ARE Dissabled ;) ")
+#             else :
+#                 return HttpResponse("YOU ARE  NOT REGISTER YET ;) ")
+#
+#         else :
+#             return HttpResponse("invalid data in form input")
+#
+#     else :
+#         form = LoginForm()
+#
+#     return render(request , 'account/login.html' , {'form': form})
 
-                if user.is_active :
-                    login(request,user)
-                    return HttpResponse("YOU ARE LOGIN ;) ")
-                else:
-                    return HttpResponse("YOU ARE Dissabled ;) ")
-            else :
-                return HttpResponse("YOU ARE  NOT REGISTER YET ;) ")
 
-        else :
-            return HttpResponse("invalid data in form input")
+from django.contrib.auth.decorators import login_required
 
-    else :
-        form = LoginForm()
+@login_required
+def dashboard(request):
+    return render(request , 'account/dashboard.html' , {'section' : 'dashboard'})
 
-    return render(request , 'account/login.html' , {'form': form})
+

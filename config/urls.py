bbde2771e18a8c9547e82cd6538fd7ec3d020779
Amir khodadi <amiramkhodadi@gmail.com>
2025-08-13 23:19:04 +0330

    nth

--- config/urls.py
+++ config/urls.py
@@ -22,7 +22,7 @@ urlpatterns = [
     path('admin/', admin.site.urls),
     path('account/',include('account.urls'))
 ]
-
+a
 if settings.DEBUG:
     urlpatterns += static(
         settings.MEDIA_URL,
